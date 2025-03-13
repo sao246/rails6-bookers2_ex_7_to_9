35a8d20e10b81646b343e71a2f3e49d4745e04af
“sao246” <195246331+sao246@users.noreply.github.com>
2025-03-13 13:39:06 +0000

    応用課題8a(SNS)[finish] DM実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,10 +3,10 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @room = Room.create
-    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
-    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
-    redirect_to "/rooms/#{@room.id}"
+    room = Room.create
+    entry1 = Entry.create(room_id: room.id, user_id: current_user.id)
+    entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: room.id))
+    redirect_to "/rooms/#{room.id}"
   end
   
   def show
@@ -14,7 +14,8 @@ class RoomsController < ApplicationController
     if Entry.where(user_id: current_user.id, room_id: @room.id).present?
       @message = @room.messages
       @message = Message.new
-      @entries = @room.entries      
+      @entries = @room.entries
+      @messages = @room.messages.order(:created_at)
     else
       redirect_back(fallback_location: root_path)
     end
