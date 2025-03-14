3c317772a15c167b14b21a446eb48639e22b5f4d
“sao246” <195246331+sao246@users.noreply.github.com>
2025-03-14 11:14:41 +0000

    応用課題8a(SNS)[finish] DM実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
 
   before_action :authenticate_user!, only: [:create]
-
+  
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.new(params.require(:message).permit(:message, :room_id).merge(user_id: current_user.id))
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
   validates :message, presence: true
   belongs_to :user
   belongs_to :room
+  validates :message, length: { in: 0..140 }
 end
